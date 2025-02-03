b75d59af40e1153742fd2ebae8b4e18b2c9b78ed
FarenLiyadi <68411667+FarenLiyadi@users.noreply.github.com>
2025-02-04 01:56:34 +0800

    second

--- src/section/HeroSection.jsx
+++ src/section/HeroSection.jsx
@@ -1,9 +1,9 @@
-import { useState, useEffect } from 'react';
-import Slider from '../component/Slider';
-import Snowflakes from '../component/Snowflakes';
+import { useState, useEffect } from "react";
+import Slider from "../component/Slider";
+import Snowflakes from "../component/Snowflakes";
 
 export default function HeroSection() {
-  const sliderData = ['1.jpg', '2.jpg', '3.jpg'];
+  const sliderData = ["1.jpg", "2.jpg", "3.jpg"];
   const [perPage, setPerPage] = useState(1);
 
   useEffect(() => {
@@ -18,29 +18,38 @@ export default function HeroSection() {
     };
 
     handleResize();
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
   return (
-    <section className='relative overflow-hidden'>
-      <div className='bg-hero'></div>
+    <section className="relative overflow-hidden">
+      <div className="bg-hero"></div>
       <Snowflakes />
-      <div className='absolute top-0 left-0 bg-black/75 w-full h-full z-[1]'></div>
-      <div className='relative z-[2] container-fluid flex flex-col items-center min-h-screen justify-center gap-4 py-8 text-white'>
-        <h1 className='font-cormorant text-[13px] md:text-[40px] text-center'>The Wedding Ceremony Of</h1>
-        <div className='container px-12 md:px-0'>
+      <div className="absolute top-0 left-0 bg-black/75 w-full h-full z-[1]"></div>
+      <div className="relative z-[2] container-fluid flex flex-col items-center min-h-screen justify-center gap-4 py-8 text-white">
+        <h1 className="font-cormorant text-[24px] md:text-[40px] text-center">
+          The Wedding Ceremony Of
+        </h1>
+        <div className="container px-12 md:px-0">
           <Slider sliderData={sliderData} perPage={perPage} autoplay={false} />
         </div>
-        <h2 className='font-gardenia text-[80px] md:text-[130px] leading-none font-light'>Hizkia & Lysa</h2>
-        <p className='font-medium font-roboto text-[12px] md:text-base'>SABTU, 23 03 2024</p>
-        <div className='container'>
-          <hr className='w-1/2 h-px mx-auto bg-white md:w-full' />
+        <h2 className="font-gardenia text-[80px] md:text-[130px] leading-none font-light">
+          Hizkia & Lysa
+        </h2>
+        <p className="font-medium font-roboto text-[12px] md:text-base">
+          SABTU, 23 03 2024
+        </p>
+        <div className="container">
+          <hr className="w-1/2 h-px mx-auto bg-white md:w-full" />
         </div>
-        <p className='font-cormorant text-center text-[11px] md:text-base'>Dan di atas semuanya itu: kenakanlah kasih, sebagai pengikat yang mempersatukan dan menyempurnakan.</p>
-        <p className='font-meath text-[25px] md:text-[35px]'>(Kolose 3 : 14)</p>
+        <p className="font-cormorant text-center text-[11px] md:text-base">
+          Dan di atas semuanya itu: kenakanlah kasih, sebagai pengikat yang
+          mempersatukan dan menyempurnakan.
+        </p>
+        <p className="font-meath text-[25px] md:text-[35px]">(Kolose 3 : 14)</p>
       </div>
     </section>
   );
